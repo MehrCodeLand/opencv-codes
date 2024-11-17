a4561f27d17c09622c1c739eb6d3b87636cb5575
Mehrshad <lastenigmas@gmail.com>
2024-11-17 16:38:09 +0330

    add face emotion to projects

--- projects/live-face-detection/main.py
+++ projects/live-face-detection/main.py
@@ -1,11 +1,12 @@
+import random
+from fer import FER
 import cv2 as cv
 import numpy as np
 import face_recognition
 import os
 
+
 cam = cv.VideoCapture(0)
-
-
 def open_webcam():
     while True:
         ret, frame = cam.read()
@@ -14,8 +15,7 @@ def open_webcam():
         if cv.waitKey(1) == ord('c'):
             local_user_images = load_images()
 
-            res = compare_image(frame , local_user_images)
-            print(res)
+            emotion_face_detecion(frame)
         cv.imshow('captures', frame)
 
         if cv.waitKey(1) == ord('q'):
@@ -49,6 +49,31 @@ def compare_image(frame , local_user_images):
             return True
         else:
             return False
+
+
+def emotion_face_detecion(frame):
+
+    detector = FER()
+    emotions = detector.detect_emotions(frame)
+
+    if emotions:
+        emotions , score = max(emotions[0]['emotions'].items() , key=lambda  item: item[1])
+        print(f"Detected emotion: {emotions} with score {score}")
+
+    face_landmarks_list = face_recognition.face_landmarks(frame)
+    for face_landmarks in face_landmarks_list:
+        left_eye = face_landmarks['left_eye']
+        right_eye = face_landmarks['right_eye']
+
+        dx = np.mean([p[0] for p in right_eye]) - np.mean([p[0] for p in left_eye])
+        if dx > 10:
+            direction = "right"
+        elif dx < -10:
+            direction = "left"
+        else:
+            direction = "straight"
+        print(f"head direction {direction}")
+
 
 open_webcam()
 
